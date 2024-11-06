355fad34eab12d56d021179fd4cc22d4fe860f70
Bineesh-pro <bineeshprofessional@gmail.com>
2024-11-06 10:21:20 +0530

    dummy payment implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
+import {provideCacheableAnimationLoader, provideLottieOptions} from 'ngx-lottie';
+import player from 'lottie-web';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),provideHttpClient()]
+  providers: [provideZoneChangeDetection({
+    eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),provideHttpClient(),provideLottieOptions({ player: () => player }),
+    provideCacheableAnimationLoader()],
 };
--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {AnimationOptions, LottieComponent} from 'ngx-lottie';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [
+    LottieComponent
+  ],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
+
+  lott_options:AnimationOptions={
+    path: 'pay_success.json',
+    loop: false,
+    autoplay: true
+  }
 
 }
